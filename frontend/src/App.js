cea0129dd3bdcd1c6ef4c1beb72c3fbe002187c8
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-07-25 18:20:04 +0200

    implement of route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
 import data from './data';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 const App = () => {
 
@@ -13,63 +16,67 @@ const App = () => {
   }
 
   return (
-    <div className="container">
+    <BrowserRouter>
+      <div className="container">
 
-      <header>
-        <div className="title">
-          <button onClick={openMenu}>
-            &#9776;
+        <header>
+          <div className="title">
+            <button onClick={openMenu}>
+              &#9776;
+            </button>
+            <a href="index.html">MERN Stack Project : E-Commerce</a>
+          </div>
+          <div className="log_cart">
+            <a href="cart.html"> Cart </a>
+            <a href="signing.html"> Log </a>
+          </div>
+        </header>
+
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            X
           </button>
-          <a href="index.html">MERN Stack Project : E-Commerce</a>
-        </div>
-        <div className="log_cart">
-          <a href="cart.html"> Cart </a>
-          <a href="signing.html"> Log </a>
-        </div>
-      </header>
-
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          X
-        </button>
-        <ul>
-          <li>
-            <a href="index.html">T-shirt</a>
-          </li>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-        </ul>
-      </aside>
-
-      <main>
-        <div className="products-container">
-          <ul className="products">
-            {
-              data.products.map(product=> 
-              <li>
-                <div className="product">
-                  <img src={product.image} alt={product.name} />
-                  <div className="product-name"><a href="product.html">{product.name}</a></div>
-                  <div className="product-brand">{product.brand}</div>
-                  <div className="product-price">{product.price}€</div>
-                  <div className="product-rating">{product.rating} stars ({product.numberReviews})</div>
-                </div>
-              </li>) 
-            }       
+          <ul>
+            <li>
+              <a href="index.html">T-shirt</a>
+            </li>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
           </ul>
-        </div>
-      </main>
+        </aside>
 
-      <footer>
-        <p>
-          MERN Stack Project : E-Commerce <br />
-          Created by Kassam Ali Asif
-        </p>
-      </footer>
+        <main>
+          <div className="products-container">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+            <ul className="products">
+              {
+                data.products.map(product=> 
+                <li>
+                  <div className="product">
+                    <img src={product.image} alt={product.name} />
+                    <div className="product-name"><a href="product.html">{product.name}</a></div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-price">{product.price}€</div>
+                    <div className="product-rating">{product.rating} stars ({product.numberReviews})</div>
+                  </div>
+                </li>) 
+              }       
+            </ul>
+          </div>
+        </main>
 
-    </div>
+        <footer>
+          <p>
+            MERN Stack Project : E-Commerce <br />
+            Created by Kassam Ali Asif
+          </p>
+        </footer>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
